649d3637f0626dbd9da44e4e99d71da92b351528
Riju-25 <rijupatel2005@gmail.com>
2025-04-09 01:28:53 +0530

    Update app_dmbi.py

--- app_dmbi.py
+++ app_dmbi.py
@@ -3,12 +3,11 @@ import pickle
 import numpy as np
 import os
 
-# Page config
 st.set_page_config(page_title="Startup Funding Prediction", page_icon="🚀", layout="centered")
 
 st.title("🚀 Startup Funding Prediction App")
 
-# Load Model & Encoders
+# Load Pickle Files
 def load_pickle(file_name):
     return pickle.load(open(os.path.join(os.path.dirname(__file__), file_name), 'rb'))
 
@@ -19,21 +18,6 @@ le_subvertical = load_pickle('le_subvertical.pkl')
 le_investors = load_pickle('le_investors.pkl')
 le_funding = load_pickle('le_funding.pkl')
 
-# Sidebar
-st.sidebar.header("About App")
-st.sidebar.markdown("""
-This app predicts the **Startup Funding Amount** based on:
-- City Location
-- Industry Vertical
-- Sub Vertical
-- Investors
-- Funding Type
-- Number of Investors
-""")
-
-st.sidebar.info("Built with ❤️ using Streamlit")
-
-# Input Fields
 st.markdown("## Enter Startup Details")
 
 city = st.selectbox("City Location", le_city.classes_)
@@ -41,9 +25,8 @@ industry = st.selectbox("Industry Vertical", le_industry.classes_)
 subvertical = st.selectbox("Sub Vertical", le_subvertical.classes_)
 investors = st.selectbox("Investors", le_investors.classes_)
 funding_type = st.selectbox("Funding Type", le_funding.classes_)
-num_investors = st.slider("Number of Investors", 1, 20, 1)
+num_investors = st.slider("Number of Investors", 1, 3, 1)
 
-# Predict Button
 if st.button("Predict Funding Amount"):
     input_data = np.array([
         le_city.transform([city])[0],
@@ -59,5 +42,4 @@ if st.button("Predict Funding Amount"):
     st.success(f"💰 Predicted Funding Amount: ₹ {np.round(prediction, 2)}")
 
 st.markdown("---")
-st.markdown("Developed by *Your Name* | Powered by Streamlit")
-
+st.markdown("Developed with ❤️ by *Your Name*")
