4b97516df9fc640e12d916d90e691f65bdf43ced
Riju-25 <rijupatel2005@gmail.com>
2025-04-09 01:41:14 +0530

    Update app_dmbi.py

--- app_dmbi.py
+++ app_dmbi.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pickle
 import numpy as np
+import pandas as pd
 
 # Load Model
 model = pickle.load(open('funding_model.pkl', 'rb'))
 
-# Load LabelEncoders
+# Load Encoders
 le_city = pickle.load(open('le_city.pkl', 'rb'))
 le_industry = pickle.load(open('le_industry.pkl', 'rb'))
 le_subvertical = pickle.load(open('le_subvertical.pkl', 'rb'))
@@ -16,26 +17,24 @@ st.title("Startup Funding Prediction App")
 
 st.markdown("### Enter Startup Details")
 
-# Dropdowns for categorical features
-city = st.selectbox("City Location", le_city.classes_)
-industry = st.selectbox("Industry Vertical", le_industry.classes_)
-subvertical = st.selectbox("Sub Vertical", le_subvertical.classes_)
-investors = st.selectbox("Investors Name", le_investors.classes_)
-funding = st.selectbox("Funding Type", le_funding.classes_)
+# Create Dropdowns with Labels
+city = st.selectbox("City Location", list(le_city.classes_))
+industry = st.selectbox("Industry Vertical", list(le_industry.classes_))
+subvertical = st.selectbox("Sub Vertical", list(le_subvertical.classes_))
+investors = st.selectbox("Investors Name", list(le_investors.classes_))
+funding = st.selectbox("Funding Type", list(le_funding.classes_))
 
-# Slider for Number of Investors
-num_investors = st.slider("Number of Investors", 1, 3, step=1)
+num_investors = st.slider("Number of Investors", 1, 3)
 
+# Prediction
 if st.button("Predict Funding Amount"):
-    # Encoding categorical values
-    city_encoded = le_city.transform([city])[0]
-    industry_encoded = le_industry.transform([industry])[0]
-    subvertical_encoded = le_subvertical.transform([subvertical])[0]
-    investors_encoded = le_investors.transform([investors])[0]
-    funding_encoded = le_funding.transform([funding])[0]
+    city_enc = le_city.transform([city])[0]
+    industry_enc = le_industry.transform([industry])[0]
+    subvertical_enc = le_subvertical.transform([subvertical])[0]
+    investors_enc = le_investors.transform([investors])[0]
+    funding_enc = le_funding.transform([funding])[0]
 
-    features = np.array([[city_encoded, industry_encoded, subvertical_encoded,
-                          investors_encoded, funding_encoded, num_investors]])
+    features = np.array([[city_enc, industry_enc, subvertical_enc, investors_enc, funding_enc, num_investors]])
 
     prediction = model.predict(features)
 
